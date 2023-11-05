44a6f31e28ea11a92e2a26f372f712f16b8e4587
Ayushi Modi <91932910+Ayushimodi129@users.noreply.github.com>
2023-11-05 11:28:11 +0530

    Update solid_rectangle.cpp

--- solid_rectangle.cpp
+++ solid_rectangle.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 using namespace std;
 int main()
-{ int m,n,i;
-cin>>m>>n;
-{for(i=0;i<m;i++)
-{
-    for(int j=0;j<n;j++)
-    { cout<<"*";}
-    cout<<"\n";
-}
-}
+{ 
+    int m,n,i;
+    cin>>m>>n;
+    for(i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+                { 
+                    cout<<"*";}
+                    cout<<"\n";
+                }
+        }
 return 0;
 }
